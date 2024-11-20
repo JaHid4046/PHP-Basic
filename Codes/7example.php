48cc95af04b525f70351dfb597c5109c53e513d5
JaHid <113750185+JaHid4046@users.noreply.github.com>
2024-11-21 01:18:55 +0600

    Update 7example.php

--- Codes/7example.php
+++ Codes/7example.php
@@ -1,29 +1,50 @@
-<? php
-		define("con", "Variables and Constants");
+<?php
+	
+	define("TITLE", "Variables and Constants");
 
-		$my_name = "Jahid";
-		$color = "Black";
-		$birth_year = 2000;
+	$my_name = "MD. Jahid Hasan";
+	$fav_color = "Black";
+	$birth_year = "2000";
 
-		date_default_timezone_set('Asia/Dhaka');
+	date_default_timezone_set('Asia/Dhaka');
 
-		$today = date('F j, Y');
-		$this_year = date('Y');
+	$today = date('F j, Y');
+	$this_year = date('Y');
 
-		$my_age = ($this_year - $birth_year);
-
-
+	$my_age = ($this_year - $birth_year);
 
 ?>
 
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html>
 <head>
-	
-	<title>HomePage</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>EXAMPLE: <?php echo TITLE ?>;</title>
+
+	<style>
+		h1{text-align: center;}
+		div{text-align: center;}
+
+
+	</style>
 </head>
 <body>
+
+	<h1>Example: <?php echo TITLE; ?></h1>
+	<hr>
+
+		<h3>Today's Date: </h3>
+		<p><?php echo "$today"; ?></p>
+		<h3>My Name: </h3>
+		<p><?php echo "$my_name"; ?></p>
+		<h3>Favourite Color: </h3>
+		<p><?php echo "$fav_color"; ?></p>
+		<h3>My Age: </h3>
+		<p><?php echo "$my_age"; ?></p>
+
+	<hr>
+	<div><small> &copy;<?php echo $this_year; ?> - <?php echo $my_name; ?></small></div>
 
 	
 
